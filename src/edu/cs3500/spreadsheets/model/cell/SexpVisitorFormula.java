92d96ab6d893e4e42180bd8c258d6e4c87c9b4e1
jlkazan <jlkazan@gmail.com>
2019-10-27 15:49:50 -0400

    updated visitor stuff

--- src/edu/cs3500/spreadsheets/model/cell/SexpVisitorFormula.java
+++ src/edu/cs3500/spreadsheets/model/cell/SexpVisitorFormula.java
@@ -1,5 +1,7 @@
 package edu.cs3500.spreadsheets.model.cell;
 
+import edu.cs3500.spreadsheets.model.Coord;
+import edu.cs3500.spreadsheets.model.cell.formula.CellReference;
 import edu.cs3500.spreadsheets.model.cell.formula.Formula;
 import edu.cs3500.spreadsheets.model.cell.formula.function.CapitalizeFunction;
 import edu.cs3500.spreadsheets.model.cell.formula.function.LessThanFunction;
@@ -50,15 +52,75 @@ public class SexpVisitorFormula implements SexpVisitor<Formula> {
       return new LessThanFunction(null);
     } else if ("CAPITALIZE".equals(s)) {
       return new CapitalizeFunction(null);
-    } else if (s.equals("A1")) {
-      // Make a single cell reference
-      return new BooleanValue(true);
-    } else if (s.equals("A1:B2")) {
-      // Make a multi cell reference
-      return new BooleanValue(true);
+    } else if (s.contains(":") && s.indexOf(":") < s.length() - 1) {
+      String cell1 = s.substring(0, s.indexOf(":"));
+      String cell2 = s.substring(s.indexOf(":") + 1);
+      if (this.validCellString(cell1) && this.validCellString(cell2)) {
+        return new CellReference(this.cellStringToCoord(cell1), this.cellStringToCoord(cell2));
+      } else {
+        // TODO: Return some sort of error value here
+        return new BooleanValue(true);
+      }
     } else {
-      throw new IllegalArgumentException("Invalid symbol: " + s);
+      if (this.validCellString(s)) {
+        Coord cellCoord = this.cellStringToCoord(s);
+        return new CellReference(cellCoord, cellCoord);
+      } else {
+        // TODO: Return some sort of error value here
+        return new BooleanValue(true);
+      }
     }
+  }
+
+  /**
+   * Determines if the given string is a valid representation of a cell. For it to be valid, it must
+   * contain only alphanumeric characters, where all the letters come before all of the numbers.
+   * @param cell The string to check
+   * @return Whether or not the string is a valid cell
+   */
+  private boolean validCellString(String cell) {
+    int finalLetter = cell.length();
+    int firstNum = 0;
+    // Iterate forwards through the string to check for final letter index
+    for (int index = 0; index < cell.length(); index += 1) {
+      char c = cell.charAt(index);
+      // If the character is neither a letter nor a number, it is not a valid cell
+      if (!(Character.isAlphabetic(c) || Character.isDigit(c))) {
+        return false;
+      } else if (Character.isAlphabetic(c)) {
+        finalLetter = index;
+      }
+    }
+    // Iterate backwards though the string to check for the earliest number index
+    for (int index = cell.length() - 1; index < 0; index -= 1) {
+      char c = cell.charAt(index);
+      // If the character is neither a letter nor a number, it is not a valid cell
+      if (!(Character.isAlphabetic(c) || Character.isDigit(c))) {
+        return false;
+      } else if (Character.isDigit(c)) {
+        firstNum = index;
+      }
+    }
+
+    return (firstNum > finalLetter);
+  }
+
+  /**
+   * Converts the given cell string to a coordinate.
+   * Invariant: The given cell is guaranteed to be a valid coordinate.
+   * @param cell The cell string
+   * @return The coordinate of the cell
+   */
+  private Coord cellStringToCoord(String cell) {
+    int lastLetter = 0;
+    for (int index = 0; index < cell.length(); index += 1) {
+      if (Character.isAlphabetic(cell.charAt(index))) {
+        lastLetter = index;
+      }
+    }
+    int column = (new Coord(0, 0).colNameToIndex(cell.substring(0, lastLetter + 1)));
+    int row = Integer.parseInt(cell.substring(lastLetter + 1));
+    return new Coord(column, row);
   }
 
   @Override
