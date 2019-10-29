5ebd8140a6a530756ad3cdce2fad427a5949da97
jlkazan <jlkazan@gmail.com>
2019-10-29 11:08:27 -0400

    implemented model methods

--- src/edu/cs3500/spreadsheets/model/SimpleSpreadsheet.java
+++ src/edu/cs3500/spreadsheets/model/SimpleSpreadsheet.java
@@ -58,7 +58,6 @@ public class SimpleSpreadsheet implements SpreadsheetModel {
      * Determines if the given string is a boolean.
      * @param contents The string
      * @return If it is a boolean
-     * TODO: IS THIS CORRECT IN ALL CASES: i.e. if there are spaces or anything else
      */
     private static boolean isBool(String contents) {
       return contents.equalsIgnoreCase("true") || contents.equalsIgnoreCase("false");
@@ -84,37 +83,73 @@ public class SimpleSpreadsheet implements SpreadsheetModel {
     }
   }
 
+  /**
+   * Creates an instance of a simple spreadsheet. This constructor can only be called via the
+   * builder.
+   * @param builder The spreadsheet builder
+   */
   private SimpleSpreadsheet(Builder builder) {
-
+    this.cells = builder.cells;
   }
 
   @Override
   public void clearCell(Coord coord) {
-
+    if (this.cells.containsKey(coord)) {
+      // TODO: Should we just remove this cell from the hashtable, since this is essentially
+      //       equivalent behavior (cells not in the table are treated as blankCells)
+      this.cells.put(coord, new BlankCell());
+    }
   }
 
   @Override
   public void setCellValue(Coord coord, String value) {
-
+    // TODO: Can implement this after fixing the builder
   }
 
   @Override
   public int getNumRows() {
-    return 0;
+    int maxRow = 0;
+    for (Coord c : this.cells.keySet()) {
+      if (c.row > maxRow) {
+        maxRow = c.row;
+      }
+    }
+    return maxRow;
   }
 
   @Override
   public int getNumColumns() {
-    return 0;
+    int maxCol = 0;
+    for (Coord c : this.cells.keySet()) {
+      if (c.col > maxCol) {
+        maxCol = c.col;
+      }
+    }
+    return maxCol;
   }
 
   @Override
+  //TODO: I think instead of using get value, we should have evaluate return a value so we don't
+  //      have to keep re-evaluating every time we edit a cell (and instead only do it every time
+  //      we need to get its value).
+  //      NOTE: This should work, because in evaluate, if there are any cell refs., we re-evaluate
+  //      the new cells, so I don't think we would run into any issues...
   public Value getValue(Coord coord) {
-    return null;
+    if (this.cells.containsKey(coord)) {
+      return this.cells.get(coord).getValue();
+    } else {
+      //TODO: What should the default value be???
+      return new StringValue("");
+    }
   }
 
   @Override
   public String getRawContents(Coord coord) {
-    return null;
+    if (this.cells.containsKey(coord)) {
+      return this.cells.get(coord).getRawContents();
+    } else {
+      // If the coordinates are not in our hashtable, the cell has not yet been edited
+      return "";
+    }
   }
 }
