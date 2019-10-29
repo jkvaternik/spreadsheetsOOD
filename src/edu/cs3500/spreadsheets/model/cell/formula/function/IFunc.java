a72aa00651555d7cbf7f4387cab8b1d3ca7eab3e
jkvaternik <jaime.kvaternik17@gmail.com>
2019-10-28 23:21:17 -0400

    Add source code

--- src/edu/cs3500/spreadsheets/model/cell/formula/function/IFunc.java
+++ src/edu/cs3500/spreadsheets/model/cell/formula/function/IFunc.java
@@ -2,6 +2,6 @@ package edu.cs3500.spreadsheets.model.cell.formula.function;
 
 import edu.cs3500.spreadsheets.model.cell.formula.Formula;
 
-public interface IFunc<Formula, R> extends edu.cs3500.spreadsheets.model.cell.formula.Formula {
-  R apply(Formula arg1, R arg2);
+public interface IFunc<A, R> extends Formula {
+  R apply(A arg1);
 }
--- src/edu/cs3500/spreadsheets/model/cell/formula/function/SumFunction.java
+++ src/edu/cs3500/spreadsheets/model/cell/formula/function/SumFunction.java
@@ -5,41 +5,94 @@ import java.util.List;
 
 import edu.cs3500.spreadsheets.model.Coord;
 import edu.cs3500.spreadsheets.model.cell.Cell;
+import edu.cs3500.spreadsheets.model.cell.formula.CellReference;
 import edu.cs3500.spreadsheets.model.cell.formula.Formula;
+import edu.cs3500.spreadsheets.model.cell.formula.value.BooleanValue;
 import edu.cs3500.spreadsheets.model.cell.formula.value.DoubleValue;
 import edu.cs3500.spreadsheets.model.cell.formula.value.ErrorValue;
+import edu.cs3500.spreadsheets.model.cell.formula.value.StringValue;
 import edu.cs3500.spreadsheets.model.cell.formula.value.Value;
 
 /**
  * Represents the sum function.
  */
-public class SumFunction extends AFunction {
+public class SumFunction implements FormulaVisitor<Double>, IFunction {
+  private List<Formula> args;
   /**
    * Constructs an instance of the SumFunction given a list of one or more arguments.
    * @param args list of arguments
    */
   public SumFunction(List<Formula> args) {
-    super(args);
+    this.args = args;
   }
 
   @Override
   public Value evaluate(Hashtable<Coord, Cell> cells) {
-    if (super.getArgs().isEmpty()) {
+    if (this.getArgs().isEmpty()) {
       throw new IllegalArgumentException("List of arguments cannot be empty.");
     }
 
     double result = 0.0;
 
-    for (Formula arg : super.getArgs()) {
-      try {
-        Value<Double> val = arg.evaluate(cells);
-        result += val.getValue();
-      } catch (IllegalArgumentException e) {
-        return new ErrorValue(new IllegalArgumentException("Function could not be evaluated."));
-      } catch (ClassCastException e) {
-        return new ErrorValue(new IllegalArgumentException("Function could not be evaluated."));
-      }
+    for (Formula arg : this.getArgs()) {
+      arg.
+
     }
     return new DoubleValue(result);
+  }
+
+  @Override
+  public Double visitDoubleValue(DoubleValue val) {
+    return null;
+  }
+
+  @Override
+  public Double visitBooleanValue(BooleanValue val) {
+    return null;
+  }
+
+  @Override
+  public Double visitStringValue(StringValue val) {
+    return null;
+  }
+
+  @Override
+  public Double visitCellReference(CellReference ref) {
+    return null;
+  }
+
+  @Override
+  public Double visitSumFunction(SumFunction func) {
+    return null;
+  }
+
+  @Override
+  public Double visitProductFunction(ProductFunction func) {
+    return null;
+  }
+
+  @Override
+  public Double visitLessThanFunction(LessThanFunction func) {
+    return null;
+  }
+
+  @Override
+  public Double visitCapitalizeFunction(CapitalizeFunction func) {
+    return null;
+  }
+
+  @Override
+  public List<Formula> getArgs() {
+    return null;
+  }
+
+  @Override
+  public void addArg(Formula arg) {
+
+  }
+
+  @Override
+  public Double apply(Formula arg1) {
+    return null;
   }
 }
