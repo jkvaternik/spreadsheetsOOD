020d63c462a656a4f028e3dfe29b53362cd093b4
jlkazan <jlkazan@gmail.com>
2019-11-09 17:59:49 -0500

    TableModel stuff

--- src/edu/cs3500/spreadsheets/view/VisualView.java
+++ src/edu/cs3500/spreadsheets/view/VisualView.java
@@ -1,9 +1,13 @@
 package edu.cs3500.spreadsheets.view;
 
+import edu.cs3500.spreadsheets.model.Coord;
 import edu.cs3500.spreadsheets.model.ViewModel;
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 /**
  * Represents a visual view for the spreadsheet. This view displays the model as a table of
@@ -27,10 +31,21 @@ public class VisualView extends JFrame implements View {
     this.setSize(500,500);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    //Sets up the Table model
+    int numRows = viewModel.getNumRows();
+    int numCols = viewModel.getNumColumns();
+    TableModel tm = new DefaultTableModel(Math.max(25, numRows), Math.max(25, numCols));
+    for (int row = 1; row <= numRows; row++) {
+      for (int col = 1; col <= numCols; col++) {
+        tm.setValueAt(viewModel.getValue(new Coord(col, row)), row - 1, col - 1);
+      }
+    }
+
+
     //Set the layout of the different components
     this.setLayout(new BorderLayout());
     this.viewModel = viewModel;
-    this.table = new JTable(10, 10);
+    this.table = new JTable(tm);
 
     this.add(table);
 
